24cc0e5284f7dd4bd21045688269e894590e8be1
tomsawyer0224 <binha0224@gmail.com>
2025-02-13 08:58:25 +0700

    formatting code

--- data.py
+++ data.py
@@ -2,10 +2,11 @@ from functools import partial
 from datasets import load_dataset, ClassLabel, Dataset
 from transformers import AutoTokenizer
 
+
 # base dataset class
 class DatasetForBert:
     def __init__(self, dataset_infor, data_files):
-        '''
+        """
         args:
             dataset_infor: dict { # in case of classification, different for other task
                 'file_type': 'csv' ('text') # str: file type (.csv or .txt)
@@ -18,74 +19,58 @@ class DatasetForBert:
                 'val': path/to/val.csv (.txt) # can be None
                 'test': path/to/test.csv (.txt) # can be None
             }
-        '''
+        """
         assert len(data_files) > 0, "OOP! 'data_files' must contain at least 1 file"
         self.dataset_infor = dataset_infor
         self.data_files = data_files
+
     def generate_datasets(self):
-        '''
+        """
         method to return 3 datasets: train, val, test
-        '''
-        train_file = self.data_files['train']
-        val_file = self.data_files['val']
-        test_file = self.data_files['test']
+        """
+        train_file = self.data_files["train"]
+        val_file = self.data_files["val"]
+        test_file = self.data_files["test"]
         has_val, has_test = False, False
         raw_dataset = load_dataset(
-            self.dataset_infor['file_type'],
-            data_files = train_file,
-            split = 'train'
+            self.dataset_infor["file_type"], data_files=train_file, split="train"
         )
         if val_file:
             val_dataset = load_dataset(
-                self.dataset_infor['file_type'],
-                data_files = val_file,
-                split = 'train'
-            ).shuffle(seed = 42)
+                self.dataset_infor["file_type"], data_files=val_file, split="train"
+            ).shuffle(seed=42)
             has_val = True
         if test_file:
             test_dataset = load_dataset(
-                self.dataset_infor['file_type'],
-                data_files = test_file,
-                split = 'train'
-            ).shuffle(seed = 42)
+                self.dataset_infor["file_type"], data_files=test_file, split="train"
+            ).shuffle(seed=42)
             has_test = True
         if has_val and has_test:
-            train_dataset = raw_dataset.shuffle(seed = 42)
+            train_dataset = raw_dataset.shuffle(seed=42)
 
         elif has_val and not has_test:
-            train_test = raw_dataset.train_test_split(
-                test_size = 0.2,
-                seed = 42
-            )
-            train_dataset = train_test['train']
-            test_dataset = train_test['test']
-            
-        elif not has_val and has_test:
-            train_val = raw_dataset.train_test_split(
-                test_size = 0.2,
-                seed = 42
-            )
-            train_dataset = train_val['train']
-            val_dataset = train_val['test']
-        else: #not has_val and not has_test
-            trainval_test = raw_dataset.train_test_split(
-                test_size = 0.15,
-                seed = 42
-            )
-            trainval = trainval_test['train']
-            test_dataset = trainval_test['test']
+            train_test = raw_dataset.train_test_split(test_size=0.2, seed=42)
+            train_dataset = train_test["train"]
+            test_dataset = train_test["test"]
 
-            train_val = trainval.train_test_split(
-                test_size = 0.2,
-                seed = 42
-            )
-            train_dataset = train_val['train']
-            val_dataset = train_val['test']
+        elif not has_val and has_test:
+            train_val = raw_dataset.train_test_split(test_size=0.2, seed=42)
+            train_dataset = train_val["train"]
+            val_dataset = train_val["test"]
+        else:  # not has_val and not has_test
+            trainval_test = raw_dataset.train_test_split(test_size=0.15, seed=42)
+            trainval = trainval_test["train"]
+            test_dataset = trainval_test["test"]
+
+            train_val = trainval.train_test_split(test_size=0.2, seed=42)
+            train_dataset = train_val["train"]
+            val_dataset = train_val["test"]
         return train_dataset, val_dataset, test_dataset
 
+
 class DatasetForBertCLS(DatasetForBert):
-    def __init__(self, dataset_infor, data_files, pre_trained = "bert-base-cased"):
-        '''
+    def __init__(self, dataset_infor, data_files, pre_trained="bert-base-cased"):
+        """
         args:
             dataset_infor: dict {
                 'file_type': 'csv' ('text') # str: file type (.csv or .txt)
@@ -98,26 +83,28 @@ class DatasetForBertCLS(DatasetForBert):
                 'val': path/to/val.csv (.txt) # can be None
                 'test': path/to/test.csv (.txt) # can be None
             }
-        '''
+        """
 
-        super().__init__(dataset_infor = dataset_infor, data_files = data_files)
-        #self.tokenizer = AutoTokenizer.from_pretrained(pre_trained, use_fast = False)
+        super().__init__(dataset_infor=dataset_infor, data_files=data_files)
+        # self.tokenizer = AutoTokenizer.from_pretrained(pre_trained, use_fast = False)
         self.tokenizer = AutoTokenizer.from_pretrained(pre_trained)
 
-        assert dataset_infor['class_names'] is not None, 'must provide class names'
+        assert dataset_infor["class_names"] is not None, "must provide class names"
         self.class_label_obj = ClassLabel(
-            num_classes = len(dataset_infor['class_names']), 
-            names = dataset_infor['class_names']
+            num_classes=len(dataset_infor["class_names"]),
+            names=dataset_infor["class_names"],
         )
 
     def map_label_str2int(self, dataset):
-        label_column_name = self.dataset_infor['label_column_name']
+        label_column_name = self.dataset_infor["label_column_name"]
         label_column_id = label_column_name + "_id"
+
         def label_name2id(example):
             example[label_column_id] = self.class_label_obj.str2int(
                 example[label_column_name]
             )
             return example
+
         dataset = dataset.map(label_name2id)
         new_features = dataset.features.copy()
         new_features[label_column_id] = self.class_label_obj
@@ -127,52 +114,57 @@ class DatasetForBertCLS(DatasetForBert):
     def prepare_datasets(self):
         train_dataset, val_dataset, test_dataset = self.generate_datasets()
 
-        #add new column (label id)
+        # add new column (label id)
         train_dataset = self.map_label_str2int(train_dataset)
         val_dataset = self.map_label_str2int(val_dataset)
         test_dataset = self.map_label_str2int(test_dataset)
 
-        #rename column to match hugging face form
+        # rename column to match hugging face form
         old_to_new = {
-            self.dataset_infor['text_column_name'] : 'text',
-            self.dataset_infor['label_column_name'] + '_id' : 'label'
+            self.dataset_infor["text_column_name"]: "text",
+            self.dataset_infor["label_column_name"] + "_id": "label",
         }
         train_dataset = train_dataset.rename_columns(old_to_new)
         val_dataset = val_dataset.rename_columns(old_to_new)
         test_dataset = test_dataset.rename_columns(old_to_new)
 
-        #tokenize function
-        tokenize = lambda example: self.tokenizer(example["text"], padding="max_length", truncation=True)
-        train_dataset = train_dataset.map(tokenize, batched = True)
-        val_dataset = val_dataset.map(tokenize, batched = True)
-        test_dataset = test_dataset.map(tokenize, batched = True)
+        # tokenize function
+        tokenize = lambda example: self.tokenizer(
+            example["text"], padding="max_length", truncation=True
+        )
+        train_dataset = train_dataset.map(tokenize, batched=True)
+        val_dataset = val_dataset.map(tokenize, batched=True)
+        test_dataset = test_dataset.map(tokenize, batched=True)
 
         return train_dataset, val_dataset, test_dataset
+
 
 class DatasetForBertQA:
     def __init__(self, name, tokenizer):
         self.name = name
-        self.train_dataset = load_dataset(name, split = 'train')
-        self.val_dataset = load_dataset(name, split = 'validation')
+        self.train_dataset = load_dataset(name, split="train")
+        self.val_dataset = load_dataset(name, split="validation")
         self.tokenizer = tokenizer
+
     def generate_datasets(self):
         raw_dataset = load_dataset(self.name)
-        train_dataset = raw_dataset['train']
-        val_dataset = raw_dataset['validation']
+        train_dataset = raw_dataset["train"]
+        val_dataset = raw_dataset["validation"]
         return train_dataset, val_dataset
+
     @staticmethod
-    def preprocess_training_examples(examples, tokenizer, max_length = 384, stride = 128):
+    def preprocess_training_examples(examples, tokenizer, max_length=384, stride=128):
         questions = [q.strip() for q in examples["question"]]
         inputs = tokenizer(
-                questions,
-                examples["context"],
-                max_length=max_length,
-                truncation="only_second",
-                stride=stride,
-                return_overflowing_tokens=True,
-                return_offsets_mapping=True,
-                padding="max_length",
-            )
+            questions,
+            examples["context"],
+            max_length=max_length,
+            truncation="only_second",
+            stride=stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length",
+        )
         offset_mapping = inputs.pop("offset_mapping")
         sample_map = inputs.pop("overflow_to_sample_mapping")
         answers = examples["answers"]
@@ -196,7 +188,10 @@ class DatasetForBertQA:
             context_end = idx - 1
 
             # If the answer is not fully inside the context, label is (0, 0)
-            if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+            if (
+                offset[context_start][0] > start_char
+                or offset[context_end][1] < end_char
+            ):
                 start_positions.append(0)
                 end_positions.append(0)
             else:
@@ -214,8 +209,9 @@ class DatasetForBertQA:
         inputs["start_positions"] = start_positions
         inputs["end_positions"] = end_positions
         return inputs
+
     @staticmethod
-    def preprocess_validation_examples(examples, tokenizer, max_length = 384, stride = 128):
+    def preprocess_validation_examples(examples, tokenizer, max_length=384, stride=128):
         questions = [q.strip() for q in examples["question"]]
         inputs = tokenizer(
             questions,
@@ -243,29 +239,26 @@ class DatasetForBertQA:
 
         inputs["example_id"] = example_ids
         return inputs
-    
+
     def prepare_datasets(self):
         train_dataset = self.train_dataset.map(
             partial(
                 self.preprocess_training_examples,
-                tokenizer = self.tokenizer,
-                max_length = 384,
-                stride = 128
+                tokenizer=self.tokenizer,
+                max_length=384,
+                stride=128,
             ),
-            batched = True,
-            remove_columns = self.train_dataset.column_names
+            batched=True,
+            remove_columns=self.train_dataset.column_names,
         )
         val_dataset = self.val_dataset.map(
             partial(
                 self.preprocess_training_examples,
-                tokenizer = self.tokenizer,
-                max_length = 384,
-                stride = 128
+                tokenizer=self.tokenizer,
+                max_length=384,
+                stride=128,
             ),
-            batched = True,
-            remove_columns = self.val_dataset.column_names
+            batched=True,
+            remove_columns=self.val_dataset.column_names,
         )
         return train_dataset, val_dataset
-    
-
-
--- train.py
+++ train.py
@@ -8,107 +8,94 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import data
 import model
 
+
 class Training:
     def __init__(self, config_file):
-        with open(config_file, 'r') as file:
+        with open(config_file, "r") as file:
             configs = yaml.safe_load(file)
         self.configs = configs
+
     def train_bert_cls(
-            self, 
-            load_from_checkpoint = None, 
-            resume_from_checkpoint = None, 
-            num_train_epochs = 3
-        ):
+        self, load_from_checkpoint=None, resume_from_checkpoint=None, num_train_epochs=3
+    ):
 
         # datasets infor
-        dataset_infor = self.configs['dataset_infor']
-        data_files = self.configs['data_files']
+        dataset_infor = self.configs["dataset_infor"]
+        data_files = self.configs["data_files"]
 
-        class_names = self.configs['dataset_infor']['class_names']
+        class_names = self.configs["dataset_infor"]["class_names"]
 
         # model infor
-        model_configs = self.configs['model_configs']
+        model_configs = self.configs["model_configs"]
 
-        pre_trained = model_configs['pre_trained']
-        num_labels = model_configs['num_labels']
+        pre_trained = model_configs["pre_trained"]
+        num_labels = model_configs["num_labels"]
 
         # training args
-        training_configs = self.configs['training_configs']
+        training_configs = self.configs["training_configs"]
 
         # prepare datasets:
         imdb_cls = data.DatasetForBertCLS(
-            dataset_infor = dataset_infor,
-            data_files = data_files,
-            pre_trained = pre_trained
+            dataset_infor=dataset_infor, data_files=data_files, pre_trained=pre_trained
         )
         train_dataset, val_dataset, test_dataset = imdb_cls.prepare_datasets()
 
-        #create model
-        id2label = {
-            k: v for k, v in enumerate(class_names)
-        }
-        label2id = {
-            k: v for v, k in enumerate(class_names)
-        }
+        # create model
+        id2label = {k: v for k, v in enumerate(class_names)}
+        label2id = {k: v for v, k in enumerate(class_names)}
 
         bert_cls = model.HFBertForCLS(
-            num_labels = num_labels,
-            id2label = id2label,
-            label2id = label2id,
-            pre_trained = pre_trained,
+            num_labels=num_labels,
+            id2label=id2label,
+            label2id=label2id,
+            pre_trained=pre_trained,
         )
 
         if load_from_checkpoint:
             bert_cls.load_checkpoint(load_from_checkpoint)
 
-        #train and test the model
+        # train and test the model
         training_args = TrainingArguments(
-            **training_configs,
-            num_train_epochs = num_train_epochs
+            **training_configs, num_train_epochs=num_train_epochs
         )
 
         trainer = Trainer(
-            model = bert_cls.model,
-            args = training_args,
-            train_dataset = train_dataset,
-            eval_dataset = val_dataset,
-            compute_metrics = bert_cls.compute_metrics
+            model=bert_cls.model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            compute_metrics=bert_cls.compute_metrics,
         )
-        #---training phase
-        trainer.train(
-            resume_from_checkpoint = resume_from_checkpoint
-        )
-        #---testing phase
-        test_results = trainer.predict(test_dataset = test_dataset)
+        # ---training phase
+        trainer.train(resume_from_checkpoint=resume_from_checkpoint)
+        # ---testing phase
+        test_results = trainer.predict(test_dataset=test_dataset)
         test_metrics = pd.DataFrame(
-                            test_results.metrics, 
-                            index = [0], 
-                            columns = ['test_loss', 'test_accuracy']
-                        )
-        test_metrics.rename(
-            columns = {"test_loss":"Test Loss", 'test_accuracy':"Test Accuracy"}, 
-            inplace = True
+            test_results.metrics, index=[0], columns=["test_loss", "test_accuracy"]
         )
-        display(HTML(test_metrics.to_html(index = False)))
+        test_metrics.rename(
+            columns={"test_loss": "Test Loss", "test_accuracy": "Test Accuracy"},
+            inplace=True,
+        )
+        display(HTML(test_metrics.to_html(index=False)))
+
     def train_bert_qa(
-            self, 
-            load_from_checkpoint = None, 
-            resume_from_checkpoint = None, 
-            num_train_epochs = 3
-        ):
-        model_config = self.configs['model_config']
+        self, load_from_checkpoint=None, resume_from_checkpoint=None, num_train_epochs=3
+    ):
+        model_config = self.configs["model_config"]
 
-        dataset_config = self.configs['dataset']
+        dataset_config = self.configs["dataset"]
 
-        training_config = self.configs['training_config']
+        training_config = self.configs["training_config"]
 
-
-        checkpoint = model_config['checkpoint']
-        dataset_name = dataset_config['dataset_name']
+        checkpoint = model_config["checkpoint"]
+        dataset_name = dataset_config["dataset_name"]
         # tokenizer
         tokenizer = AutoTokenizer.from_pretrained(checkpoint)
         # dataset
-        dataset_for_bert_qa = data.DatasetForBertQA(name = dataset_name, tokenizer = tokenizer)
+        dataset_for_bert_qa = data.DatasetForBertQA(
+            name=dataset_name, tokenizer=tokenizer
+        )
         train_dataset, val_dataset = dataset_for_bert_qa.prepare_datasets()
         # model
         bert_for_qa = model.HFBertForQA(checkpoint)
@@ -118,44 +105,40 @@ class Training:
         data_collator = DefaultDataCollator()
 
         training_args = TrainingArguments(
-            **training_config,
-            num_train_epochs = num_train_epochs
+            **training_config, num_train_epochs=num_train_epochs
         )
         trainer = Trainer(
-            model = bert_for_qa.model,
-            args = training_args,
-            train_dataset = train_dataset,
-            eval_dataset = val_dataset,
-            tokenizer = tokenizer,
-            data_collator=data_collator
+            model=bert_for_qa.model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
         )
-        trainer.train(
-            resume_from_checkpoint = resume_from_checkpoint
-        )
+        trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--bert_type', default = 'cls')
-    parser.add_argument('--config_file', default = 'config/cls_configs.yaml')
-    parser.add_argument('--num_train_epochs', type = float, default = 3)
-    parser.add_argument('--load_from_checkpoint', default = None)
-    parser.add_argument('--resume_from_checkpoint', default = None)
+    parser.add_argument("--bert_type", default="cls")
+    parser.add_argument("--config_file", default="config/cls_configs.yaml")
+    parser.add_argument("--num_train_epochs", type=float, default=3)
+    parser.add_argument("--load_from_checkpoint", default=None)
+    parser.add_argument("--resume_from_checkpoint", default=None)
 
     args = parser.parse_args()
 
-    training_bert = Training(config_file = args.config_file)
-    if args.bert_type == 'cls':
+    training_bert = Training(config_file=args.config_file)
+    if args.bert_type == "cls":
         training_bert.train_bert_cls(
-            load_from_checkpoint = args.load_from_checkpoint,
-            resume_from_checkpoint = args.resume_from_checkpoint,
-            num_train_epochs = args.num_train_epochs
+            load_from_checkpoint=args.load_from_checkpoint,
+            resume_from_checkpoint=args.resume_from_checkpoint,
+            num_train_epochs=args.num_train_epochs,
         )
-    if args.bert_type == 'qa':
+    if args.bert_type == "qa":
         training_bert.train_bert_qa(
-            load_from_checkpoint = args.load_from_checkpoint,
-            resume_from_checkpoint = args.resume_from_checkpoint,
-            num_train_epochs = args.num_train_epochs
+            load_from_checkpoint=args.load_from_checkpoint,
+            resume_from_checkpoint=args.resume_from_checkpoint,
+            num_train_epochs=args.num_train_epochs,
         )
-
-
